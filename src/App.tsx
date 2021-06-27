e8166df4709833397681f5e8232bcbfbf6e3c12e
Divyanshu Gupta <guptadv17@gmail.com>
2021-06-27 15:28:46 +0530

    Routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import {lazy, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+import * as ROUTES from './constants/routes' 
+
+
+const LandingPage = lazy( () => import('./pages/landing-page'))
+const NotFound = lazy( () => import('./pages/not-found'))
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+        <Suspense fallback={<p>Loading....</p>}>
+            <Switch>
+                <Route path={'/'} exact component = {LandingPage}/>
+                <Route path={ROUTES.NOTFOUND} exact component = {NotFound}/>
+            </Switch>
+        </Suspense>
+    </Router>
   );
 }
 
--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,3 +1,16 @@
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../../constants/routes'
+
 export default function LandingPage(){
+
+    return (
+        <div>
+            This is the Kōen.
+            <br/>
+            <Link to={ROUTES.NOTFOUND}>
+                <button> take me to not found</button>
+            </Link>
+        </div>
+    )
     
 }
