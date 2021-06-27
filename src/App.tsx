00e7bd5f35b126448e264d1ec63556f684ec00cf
Divyanshu Gupta <guptadv17@gmail.com>
2021-06-27 19:52:56 +0530

    moved images and adding landing page html

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Suspense } from 'react'
+import { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import routes from './constants/routes' 
 
--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -1,16 +1,56 @@
 import { Link } from 'react-router-dom'
 import { NOTFOUND_PATH } from '../../constants/paths'
 
+import ImageStartJourney from './../../assets/images/start_journey.svg';
+import ImageLadyBird from './../../assets/images/ladybird.png'
+import ImageLookForCreators from './../../assets/images/look_for_creators.svg'
 export default function LandingPage(){
 
     return (
-        <div>
-            This is the Kōen.
-            <br/>
-            <Link to={NOTFOUND_PATH}>
-                <button type="button" className="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"> take me to not found</button>
-            </Link>
+        <div className="container ">
+
+    <form className="">
+        <div className="col col-md-4">
+            <input type="text" placeholder="pagename"/>
         </div>
+        <div className="col col-md-4">
+            <button className="button" type="submit">Create</button>
+        </div>
+    </form>
+
+    <div className="row row-cols-1">
+
+        <div className="col">
+            <img src={ImageStartJourney} alt="start journey"/>
+        </div>
+
+        <div className="col">
+            <h2> Stir up support on a truly global platform. </h2>
+            <p className="">Koen empowers creators to accept support,
+                memberships, and build a direct relationship with their fans, from anywhere across the planet.</p>
+        </div>
+
+        <div className="col mx-auto">
+            <div className="ladybird mx-auto">
+                <img src={ImageLadyBird} className="img-responsive"/>
+                <div className="overlay-text">
+
+                    <h3> Create your page in a jiffy. </h3> 
+                    <p> Its absolutely free. No gas fees, No extra charges </p>
+                </div>
+            </div>
+        </div>
+
+        <div className="col look-for-creator">
+            <img src={ImageLookForCreators}/>
+        </div>
+
+    </div>
+    
+    <div className="footer">
+    </div>
+</div>
+
     )
     
 }
