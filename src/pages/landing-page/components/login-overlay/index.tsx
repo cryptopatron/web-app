e365ff77963a1b0c75a45b9d2263baeb3d42e293
Divyanshu Gupta <guptadv17@gmail.com>
2021-06-28 23:21:41 +0530

    google auth added

--- src/pages/landing-page/components/login-overlay/index.tsx
+++ src/pages/landing-page/components/login-overlay/index.tsx
@@ -1,20 +1,82 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
 import ImageLoginWoman from "./../../../../assets/images/login-woman.svg"
 import ImageGoogleIcon from "./../../../../assets/images/google-icon.svg"
 import ImageMetamaskIcon from "./../../../../assets/images/metamask-icon.svg"
+import { refreshTokenSetup } from "./refreshTokenSetup";
+
+import { NOTFOUND_PATH }  from './../../../../constants/paths'
+
+const clientId =
+  '116852492535-37n739s732ui71hkfm19n5r3agv6g9c5.apps.googleusercontent.com';
 
 export default function LoginOverlayComponent() {
+
+    const history = useHistory();
+
+    const googleLogIn = async googleData => {
+        const res = await fetch(window.origin + "/auth/google/jwt", {
+            method: "POST",
+            body: JSON.stringify({
+                token: googleData.tokenId
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        const data = await res.json()
+        if(data.error) throw new Error(data.error)
+
+        if(data){
+            history.push(NOTFOUND_PATH);
+        }
+    }
+
+    const responseGoogleOnSuccess = (response) => {
+        console.log(JSON.stringify(response))
+        googleLogIn(response)
+        refreshTokenSetup(response)
+    }
+
+
+    const responseGoogleOnFailure = (response) => {
+        console.log(JSON.stringify(response))
+    }
+
+    const logout = () => {
+        console.log("logged out")
+    }
+
     return (
         <>
-        <div>
-        <div> <img src={ImageLoginWoman} alt="desperation"/>  </div>
-        <div className="">
-            <h3 className="">Ready. Set. Koen.</h3>
-            <button type="button" className="" ><span><img src={ImageGoogleIcon} alt="google"/></span> login with Google</button>
-            <button type="button" className="" ><span><img src={ImageMetamaskIcon} alt="metamask"/></span> login with MetaMask</button>
-            <button type="button" className="" >Google Sign out (temp)</button>
-        </div>
-    </div>
+            <div className="flex flex-row">
+                <div className="">
+                    <img className="hidden sm:block" src={ImageLoginWoman} alt="desperation" />
+                </div>
+                <div className="flex flex-col mx-auto justify-center">
+                    <h3 className="text-center">Ready. Set. Koen.</h3>
+                    <GoogleLogin
+                        clientId={clientId}
+                        render={renderProps => (
+                            <button onClick={renderProps.onClick} disabled={renderProps.disabled} type="button" className="btn-sec" ><span><img className="inline-block w-7 mx-2" src={ImageGoogleIcon} alt="google" /></span> login with Google</button>
+                        )}
+                        buttonText="Login"
+                        onSuccess={responseGoogleOnSuccess}
+                        onFailure={responseGoogleOnFailure}
+                        cookiePolicy={'single_host_origin'}
+                        isSignedIn={true}
+                    />
+
+                    <button type="button" className="btn-sec" ><span><img className="inline-block w-7 mx-2" src={ImageMetamaskIcon} alt="metamask" /></span> login with MetaMask</button>
+                    <GoogleLogout
+                        clientId={clientId}
+                        buttonText="Logout"
+                        onLogoutSuccess = {logout}
+                    >
+                    </GoogleLogout>
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/landing-page/components/navbar/index.tsx
+++ src/pages/landing-page/components/navbar/index.tsx
@@ -1,55 +1,95 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react'
 import logo from '../../../../assets/images/Logo(Latest).jpg';
-import Modal from "@material-tailwind/react/Modal";
-import ModalHeader from "@material-tailwind/react/ModalHeader";
-import ModalBody from "@material-tailwind/react/ModalBody";
-import ModalFooter from "@material-tailwind/react/ModalFooter";
+import { Dialog, Transition } from '@headlessui/react'
 
 import LoginOverlayComponent from './../login-overlay/'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
-export default function NavbarComponent(){
+export default function NavbarComponent() {
     const [showLoginOverlay, setShowLoginOverlay] = useState(false);
+
+    function closeModal() {
+        setShowLoginOverlay(false)
+      }
+    
+      function openModal() {
+        setShowLoginOverlay(true)
+      }
+
     return (
         <>
-          <nav className="relative flex items-center justify-between px-2 py-3 bg-white mb-5">
-            <div className="container px-4 mx-auto flex items-center justify-between">
-              <div className="w-auto relative flex justify-start static lg:block">
-                <a
-                  className="leading-relaxedmr-4 py-2 whitespace-nowrap"
-                  href="#pablo"
-                >
-                  <img src={logo} alt="Navbar Kōen logo" />
-                </a>
+            <nav className="relative flex items-center justify-between px-2 py-3 bg-white mb-5">
+                <div className="container px-4 mx-auto flex items-center justify-between">
+                    <div className="w-auto relative flex justify-start static lg:block">
+                        <a
+                            className="leading-relaxedmr-4 py-2 whitespace-nowrap"
+                            href="#pablo"
+                        >
+                            <img src={logo} alt="Navbar Kōen logo" />
+                        </a>
+                    </div>
+                    <div className="items-center">
+                        <ul className="ml-auto">
+                            <li className="nav-item">
+                                <button
+                                    className="btn-main"
+                                    onClick={() => {
+                                        openModal()
+                                    }}
+                                >
+                                    <span>Sign In</span>
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <Transition appear show={showLoginOverlay} as={Fragment}>
+        <Dialog
+          as="div"
+          className="fixed inset-0 z-10 overflow-y-auto"
+          onClose={closeModal}
+        >
+          <div className="min-h-screen px-0 text-center">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-50"
+              leaveTo="opacity-0"
+            >
+                <Dialog.Overlay className="fixed inset-0 bg-black opacity-50 " />
+            </Transition.Child>
+            
+            {/* This element is to trick the browser into centering the modal contents. */}
+            <span
+              className="inline-block h-screen align-middle"
+              aria-hidden="true"
+            >
+              &#8203;
+            </span>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <div className="fixed mx-auto bottom-0 inset-x-0 w-full sm:bottom-1/3 sm:top-1/3 max-w-2xl p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-t-2xl sm:rounded-2xl">
+                
+                <LoginOverlayComponent/>
               </div>
-              <div className="items-center">
-                <ul className="ml-auto">
-                  <li className="nav-item">
-                    <button
-                      className="btn-main"
-                      onClick={() => {
-                          setShowLoginOverlay(true)
-                      }}
-                    >
-                      <span>Sign In</span>
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-          
-          <Modal size="regular" active={showLoginOverlay} toggler={() => setShowLoginOverlay(false)}>
-                <ModalHeader toggler={() => setShowLoginOverlay(false)}>
-                </ModalHeader>
-                <ModalBody>
-                    <LoginOverlayComponent/>
-                </ModalBody>
-                <ModalFooter>
-                </ModalFooter>
-            </Modal>
+            </Transition.Child>
+          </div>
+        </Dialog>
+      </Transition>
 
         </>
-      );
+    );
 }
