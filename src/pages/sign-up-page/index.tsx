b25d67969552e6b425a4c1dae8184340cda8bde5
hppranaav <hppranaav02@gmail.com>
2021-07-01 00:26:44 +0530

    Add steps design

--- src/pages/sign-up-page/index.tsx
+++ src/pages/sign-up-page/index.tsx
@@ -1,17 +1,20 @@
 import { useState } from "react";
 import Step1Component from "./step1";
+import Step2Component from "./step2";
+import Step3Component from "./step3";
 
 export default function SignUpPage(){
     
-    let [count, setCount] = useState<number>(1);
+    let [step, setStep] = useState<number>(1);
 
-    const somFunc = () => {
-        setCount(() => count++) 
+    const moveToStep = () => {
+        setStep(() => step++) 
     }
     return (
         <div>
-            <Step1Component clickFunc={somFunc}/>
-            Hello {count}
+            <Step1Component step={step} clickFunc={moveToStep}/>
+            {/* <Step2Component /> */}
+            {/* <Step3Component /> */}
         </div>
     )
 }
