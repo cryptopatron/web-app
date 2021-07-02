5008e7e04b35addda8357493018a82f498b9de2c
Divyanshu Gupta <guptadv17@gmail.com>
2021-07-02 18:17:22 +0530

    added logginContext

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import routes from './constants/routes'
 import { LANDINGPAGE } from './constants/routes';
 import "@material-tailwind/react/tailwind.css";
 import UserContext from "./contexts/user"
-import LoggedInUserContext, {creator} from './contexts/logged-in-user';
+import LoggedInUserContext, { creator } from './contexts/logged-in-user';
 import useToken from './hooks/useToken';
 import { useState } from 'react';
 
@@ -14,32 +14,36 @@ function App() {
 
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
     const [user, setUser] = useState<Creator>(creator)
-    const {token, setToken} = useToken({isLoggedIn})
-    
+    const { token, setToken } = useToken({ isLoggedIn })
+
     return (
-        <UserContext.Provider value={{isLoggedIn, setIsLoggedIn, token, setToken}}>
-            <Router>
-                <Suspense fallback={<p>Loading....</p>}>
-                    <Switch>
-                        {routes.map((route) => (
-                            // Added ccondtions for route guarding 
-                            (route.protected) ? (
-                                <Route
-                                    path={LANDINGPAGE.path}
-                                    component={LANDINGPAGE.component}
-                                    exact={LANDINGPAGE.exact}
-                                />
-                            ) :
-                                (<Route
-                                    path={route.path}
-                                    component={route.component}
-                                    exact={route.exact}
-                                />)
-                        ))}
-                    </Switch>
-                </Suspense>
-            </Router>
+
+        <UserContext.Provider value={{ isLoggedIn, setIsLoggedIn, token, setToken }}>
+            <LoggedInUserContext.Provider value={{ user, setUser }}>
+                <Router>
+                    <Suspense fallback={<p>Loading....</p>}>
+                        <Switch>
+                            {routes.map((route) => (
+                                // Added ccondtions for route guarding 
+                                (route.protected) ? (
+                                    <Route
+                                        path={LANDINGPAGE.path}
+                                        component={LANDINGPAGE.component}
+                                        exact={LANDINGPAGE.exact}
+                                    />
+                                ) :
+                                    (<Route
+                                        path={route.path}
+                                        component={route.component}
+                                        exact={route.exact}
+                                    />)
+                            ))}
+                        </Switch>
+                    </Suspense>
+                </Router>
+            </LoggedInUserContext.Provider>
         </UserContext.Provider>
+
     );
 }
 
