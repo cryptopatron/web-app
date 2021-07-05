2d4a68f6667932931f92678d05cb47ae7b0ccdf6
Divyanshu Gupta <guptadv17@gmail.com>
2021-07-05 22:51:09 +0530

    navbar

--- src/pages/dashboard-page/components/side-panel/index.tsx
+++ src/pages/dashboard-page/components/side-panel/index.tsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import ImageDefaultProfile1 from './../../../../assets/images/default_profile_1.svg'
-
+import {Link} from 'react-router-dom'
 export default function SidePanelComponent({ creator }) {
     return (
         <div>
             
             {/* pictture and edit button */}
             <div>
-                <div className=" flex mx-auto justify-center w-32 h-32 rounded-full bg-primary-hover">
+                <div className=" flex mx-auto mt-8 justify-center w-32 h-32 rounded-full bg-primary-hover">
                     <img className="w-2/5 transform translate-y-3" src={ImageDefaultProfile1} alt="place_holder_image" />
                 </div>
                 <div className="text-center">
-                    <p>Edit Profile</p>
+                <Link to={`/${creator.pageName}`}> <p className="text-sm my-4 font-light">View Profile</p> </Link> 
                 </div>
 
             </div>
 
             {/* welcom name */}
-            <div className="text-center">
-                <h3>Welcome, Shaw Man{creator.name}</h3>
+            <div className="text-center my-1">
+                <h3>Welcome, {creator.name}</h3>
             </div>
 
             {/* supporters count */}
-            <div className="text-center">
+            <div className="text-center my-4">
                 <p>0</p>
                 <p className="">Supporters</p>
             </div>
--- src/services/backendService.ts
+++ src/services/backendService.ts
@@ -65,3 +65,21 @@ export const updateUserProfile = async (name, bio, token) => {
         return false
     }
 }
+
+
+export const getTransactionByPageName = async (pageName) => {
+
+    const endpoint = window.origin + `/api/v1/users/pageName/${pageName}` //window.origin
+    const res  = await fetch(endpoint, {
+        method: "GET",
+    });
+
+    const status = await res.status
+
+    if(status !== 200){
+        return null
+    }
+
+    const response = await res.json()
+    return response
+}
