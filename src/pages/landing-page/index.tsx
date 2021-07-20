1165829a39d946797712d76898aafe42ed98e309
Divyanshu Gupta <guptadv17@gmail.com>
2021-07-20 05:51:36 +0530

    landing page clean up

--- src/pages/landing-page/index.tsx
+++ src/pages/landing-page/index.tsx
@@ -7,9 +7,9 @@ import UserContext from "../../contexts/user";
 
 // images
 import ImageStartJourney from "./../../assets/images/start_journey.svg";
-import ImageLadyBird from "./../../assets/images/ladybird.png";
-import ImageLookForCreators from "./../../assets/images/look_for_creators.svg";
+import ImageBackground_1 from "./../../assets/images/background_1.svg"
 import FooterComponent from "../../components/footer";
+import Rectangle from "./../../assets/images/rect.svg"
 
 export default function LandingPage() {
     const { token, setToken, isLoggedIn } = useContext(UserContext);
@@ -71,80 +71,89 @@ export default function LandingPage() {
                     <div className="text-left w-11/12 max-w-2xl">
                         <p className="mt-10 text-4xl font-semibold  md:text-5xl">
                             {" "}
-                            Get your creative work funded <br /> <span className="text-4xl font-normal">on a truly global platform</span>{" "}
+                            <span className="leading-tight">Get your <div className=" inline-block "> <div className="z-10 relative"> creat</div> <div className="z-0"><img className="absolute transform -translate-y-4 md:-translate-y-5 -translate-x-8 w-32 h-3 md:w-40 md:h-4" src={Rectangle} alt="" /></div></div>ive work funded</span> <br /> <span className="text-4xl font-normal">on a truly global platform</span>{" "}
                         </p>
-                        <p className=" md:hidden my-4 md:mt-8 font-medium text-gray-600">
+                    </div>
+                </div>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 md:mt-6">
+                    <div className="mx-auto w-11/12 md:row-start-2 md:justify-self-end transform md:translate-x-64">
+                        <p className=" my-4 md:mt-8 font-medium text-gray-600 max-w-sm">
                             Accept financial support, create
                             economies, and play value-based games with your fans, from
                             anywhere across the planet.
                         </p>
                     </div>
-                </div>
-                <form
-                    className="flex flex-col md:flex-row justify-center items-center mt-5"
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        clickSignIn();
-                    }}
-                >
-                    <input
-                        className="input-main w-full mt-3 sm:w-4/5 md:w-3/5 max-w-lg text-center md:text-left md: pl-8"
-                        type="text"
-                        aria-label="Enter your pagename"
-                        placeholder="pagename"
-                        value={pageName}
-                        onChange={(e) => setPageName(e.target.value)}
-                    />
+                    <div className="md:col-span-2 md:row-start-1">
+                        <form
+                            className="flex flex-col md:flex-row justify-center items-center"
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                clickSignIn();
+                            }}
+                        >
+                            <input
+                                className="input-main w-full mt-3 sm:w-4/5 md:w-3/5 max-w-lg text-center md:text-left md: pl-8"
+                                type="text"
+                                aria-label="Enter your pagename"
+                                placeholder="pagename"
+                                value={pageName}
+                                onChange={(e) => setPageName(e.target.value)}
+                            />
 
-                    <button className="btn-main w-full mx-3 mt-3 sm:w-4/5 md:w-28">
-                        Create
-                    </button>
-                </form>
-
-                <div className="flex flex-col md:flex-row justify-center items-center mb-10">
-                    <div className="flex order-first md:order-last justify-center ">
+                            <button className="btn-main w-full mx-3 mt-3 sm:w-4/5 md:w-28">
+                                Create
+                            </button>
+                        </form>
+                    </div>
+                
+                    <div className="flex justify-center md:justify-self-start">
                         <img
-                            className=" w-4/5 transform translate-x-14 lg:translate-x-24 "
+                            className=" w-48 transform translate-x-14 md:translate-x-60 "
                             src={ImageStartJourney}
                             alt="start journey"
                         />
                     </div>
 
-                    <div className="max-w-md transform -translate-y-5 text-center md:text-left">
-
-                        <p className="hidden md:inline-block font-medium text-gray-600">
-                            Accept financial support, create
-                            economies, and play value-based games with your fans, from
-                            anywhere across the planet.
-                        </p>
-                    </div>
                 </div>
+                
             </div>
 
-            <div className="bg-primary-hover flex-grow ">
-                <div className="container mx-auto py-12">
-                    <div className="flex flex-col justify-center max-w-2xl mx-auto">
-                        <div className="flex justify-center mb-6">
-                            <h3> How does it work?</h3>
+            <div className=" flex-grow ">
+                <div className="bg-repeat-x" style={{ backgroundImage: `url(${ImageBackground_1})` }}>
+                    <div className="container mx-auto py-12">
+                        <div className="flex flex-col justify-center max-w-2xl mx-auto mt-32">
+                            <div className="flex justify-center mb-6">
+                                <h3> How does it work?</h3>
+                            </div>
+
+                            <div className="grid sm:grid-cols-3 gap-6 sm:h-44">
+
+                                {howToContent.map((card, index) => (
+                                    <div key={index} className=" grid text-left shadow-lg bg-white rounded-md p-4 md:-translate-y-14">
+                                        <div className="justify-self-start">
+                                            <h2 className=""> {card.title} </h2>
+                                        </div>
+                                        <div className="sm:justify-self-end">
+                                            <p className="mt-2 font-semibold text-gray-600">
+                                                {card.content}
+                                            </p>
+                                        </div>
+                                    </div>))}
+
+
+                            </div>
+
+                            <div className="flex flex-col justify-center mt-8 mb-2">
+                                <div className="text-center">
+                                    <h3> Don't have a crypto-wallet?</h3>
+                                </div>
+                                <div className="text-center">
+                                    <p className="text-lg">We'll make one for you!</p>
+                                </div>
+                            </div>
+
                         </div>
-
-                        <div className="grid sm:grid-cols-3 gap-6 sm:h-44">
-
-                            {howToContent.map((card, index) => (
-                                <div key={index} className=" grid text-left shadow-lg bg-white rounded-md p-4 md:-translate-y-14">
-                                    <div className="justify-self-start">
-                                        <h2 className=""> {card.title} </h2>
-                                    </div>
-                                    <div className="sm:justify-self-end">
-                                        <p className="mt-2 font-semibold text-gray-600">
-                                            {card.content}
-                                        </p>
-                                    </div>
-                                </div>))}
-
-
-                        </div>
-
                     </div>
                 </div>
             </div>
