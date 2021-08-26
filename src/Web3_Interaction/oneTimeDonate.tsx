527b002f1d6c442ed2443bd0a24d18d5cb7c448d
prampey <prudhvi.rampey@gmail.com>
2021-08-26 21:22:51 +0530

    Fix empty wallet address for Recipient
    - Metamask address prioritized before generated wallet
    - Pretty formatted some files
    - pageName used to refer to creator after complete txn

--- src/Web3_Interaction/oneTimeDonate.tsx
+++ src/Web3_Interaction/oneTimeDonate.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Metamask_connect_and_execute } from "./walletConnect";
 import { txn_link_msg, get_currency_decimals_by_address } from "./Utils";
-import abis from './abis.json';
-import networks from './networks.json';
+import abis from "./abis.json";
+import networks from "./networks.json";
 
 // executes one time donation through Metamask.
 export async function Metamask_OneTime_Donation(setMsg, setErr, params) {
-    params["default_gas_price"] = networks[params.network].default_gas_price;
-    Metamask_connect_and_execute(setMsg, setErr, networks[params.network], donate, params);
+	params["default_gas_price"] = networks[params.network].default_gas_price;
+	Metamask_connect_and_execute(
+		setMsg,
+		setErr,
+		networks[params.network],
+		donate,
+		params
+	);
 }
-
 
 // params = {
 //     "recipient_name": "name",
@@ -21,51 +26,96 @@ export async function Metamask_OneTime_Donation(setMsg, setErr, params) {
 //     "network": "mumbai"
 //     }
 // One time donation to recipient_address
-async function donate(setMsg, setErr, web3Provider, ethereum, accounts, params) {
-    web3Provider.eth.defaultAccount = accounts[0];
+async function donate(
+	setMsg,
+	setErr,
+	web3Provider,
+	ethereum,
+	accounts,
+	params
+) {
+	web3Provider.eth.defaultAccount = accounts[0];
 
-    let payment_token = new web3Provider.eth.Contract(
-        abis["erc20_contract"],
-        params["payment_token_address"]
-    );
+	let payment_token = new web3Provider.eth.Contract(
+		abis["erc20_contract"],
+		params["payment_token_address"]
+	);
 
-    let not_called = true;
-    payment_token.methods.balanceOf(accounts[0]).estimateGas({
-        from: accounts[0]
-    }).then(function(gasAmount) {
-        payment_token.methods.balanceOf(accounts[0]).call({
-            from: accounts[0]
-        }).then(function(balance) {
-            const token_decimals = get_currency_decimals_by_address(params.network, params.payment_token_address);
-            let int_balance = BigInt(balance + "0".repeat(token_decimals));
-            let int_donation = BigInt(params.donation_amount);
-            if (int_balance >= int_donation) {
-                // user has enough tokens to make the donation
-                payment_token.methods.transfer(params["recipient_address"], params["donation_amount"])
-                    .send({
-                        from: accounts[0],
-                        gasPrice: params["default_gas_price"], gas: 3*gasAmount
-                    })
-                    .on('error', function (error) {
-                        setErr("Failed to get permission to send donation");
-                    })
-                    .on('transactionHash', function (txn_hash) {
-                        setMsg(txn_link_msg(txn_hash, params.network, "Sending Donation..."));
-                    })
-                    .on('confirmation', function (confirmationNumber, receipt) {
-                        if (confirmationNumber === 2 && (not_called)) {
-                            not_called = false;
-                            let msg = params["recipient_name"] + " has received your Donation!";
-                            setMsg(txn_link_msg(receipt.transactionHash, params.network, msg));
-                        }
-                    })
-            } else {
-                // user doesn't have enough tokens to make the donation
-                setErr("Insufficient funds to make donation");
-            }
-        })
-    }).catch((e) =>{
-        setErr("Failed to get permission to send donation");
-    })
-
+	let not_called = true;
+	payment_token.methods
+		.balanceOf(accounts[0])
+		.estimateGas({
+			from: accounts[0],
+		})
+		.then(function (gasAmount) {
+			payment_token.methods
+				.balanceOf(accounts[0])
+				.call({
+					from: accounts[0],
+				})
+				.then(function (balance) {
+					const token_decimals = get_currency_decimals_by_address(
+						params.network,
+						params.payment_token_address
+					);
+					let int_balance = BigInt(
+						balance + "0".repeat(token_decimals)
+					);
+					let int_donation = BigInt(params.donation_amount);
+					if (int_balance >= int_donation) {
+						// user has enough tokens to make the donation
+						payment_token.methods
+							.transfer(
+								params["recipient_address"],
+								params["donation_amount"]
+							)
+							.send({
+								from: accounts[0],
+								gasPrice: params["default_gas_price"],
+								gas: 3 * gasAmount,
+							})
+							.on("error", function (error) {
+								setErr(
+									"Failed to get permission to send donation"
+								);
+							})
+							.on("transactionHash", function (txn_hash) {
+								setMsg(
+									txn_link_msg(
+										txn_hash,
+										params.network,
+										"Sending Donation..."
+									)
+								);
+							})
+							.on(
+								"confirmation",
+								function (confirmationNumber, receipt) {
+									if (
+										confirmationNumber === 2 &&
+										not_called
+									) {
+										not_called = false;
+										let msg =
+											params["recipient_name"] +
+											" has received your Donation!";
+										setMsg(
+											txn_link_msg(
+												receipt.transactionHash,
+												params.network,
+												msg
+											)
+										);
+									}
+								}
+							);
+					} else {
+						// user doesn't have enough tokens to make the donation
+						setErr("Insufficient funds to make donation");
+					}
+				});
+		})
+		.catch((e) => {
+			setErr("Failed to get permission to send donation");
+		});
 }
